1bd730566debf30d6b08d5861c1c6442782290bf
chiaki-taro <chias.926@gmail.com>
2024-06-18 13:32:53 +0900

    ruby24

---
+++ ruby_24.rb
@@ -0,0 +1,22 @@
+# Aさんは普段土日が休みの仕事に就いており、休みの日は遅くまで寝ていたいと考えています。
+# そこで、Aさんのために「その日が遅くまで寝ていられるかどうか」を判断する、sleep_inメソッドを実装しましょう。
+
+# 第一引数の値では「平日かどうか」、第二引数の値では「休暇かどうか」をtrueまたはfalseを用いて以下のように表します。
+
+# 第一引数がtrue（平日である）または、第二引数がtrue（休暇である）の場合はtrueと出力する
+# 第一引数がfalse（平日でない）または、第二引数がtrue（休暇である）の場合はtrueと出力する
+# 第一引数がtrue（平日である）または、第二引数がfalse（休暇でない）の場合はfalseと出力する
+# 第一引数がfalse（平日でない）または、第二引数がfalse（休暇でない）の場合はtrueと出力する
+
+def sleep_in(is_weekday, is_vacation)
+  if !is_weekday || is_vacation 
+    puts true
+  else
+    puts false
+  end
+end
+
+# 呼び出し例
+sleep_in(false, false)
+sleep_in(true, false)
+sleep_in(false, true)
