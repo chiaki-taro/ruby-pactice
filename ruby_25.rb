b5cdd0e84ac91d6917e88f557080089062061a2a
chiaki-taro <chias.926@gmail.com>
2024-06-19 10:47:43 +0900

    ruby25

---
+++ ruby_25.rb
@@ -0,0 +1,31 @@
+# 二桁以上の整数を入力すると、十の位と一の位の数字の足し算、十の位と一の位の数字の掛け算をそれぞれ行い、最後に2つの結果を足し合わせて出力するプログラムをRubyで作成してください。
+
+def addition(a, b)
+  # 10の位と1の位の数字に対して、足し算を行う
+  a + b
+end
+
+def multiplication(a,b)
+  # 10の位と1の位の数字に対して、掛け算を行う
+  a * b
+end
+
+def slice_num(num)
+  # 10の位
+  tens_place = (num / 10) % 10
+  # 1の位
+  ones_place = num % 10
+  return tens_place, ones_place
+end
+
+puts "二桁の整数を入力してください"
+input = gets.to_i
+X, Y = slice_num(input)
+
+#  additionメソッドにX,Yを引数として渡し、処理結果を変数add_resultに代入する。
+add_result = addition(X, Y)
+
+#  multiplicationメソッドにX,Yを引数として渡し、処理結果を変数multiple_resultに代入する。
+multiple_result = multiplication(X, Y)
+
+puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
