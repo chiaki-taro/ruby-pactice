7587f2dd294973cf51d1bbc83af728b3a7e54a45
chiaki-taro <chias.926@gmail.com>
2024-06-26 12:03:23 +0900

    ruby29

---
+++ ruby_29.rb
@@ -0,0 +1,19 @@
+# 非常に有名なプログラミングの問題です。
+# 1〜100までの数字をターミナルに出力してください。
+# ただし、「3の倍数」のときは数字の代わりに文字列でFizzと、「5の倍数」のときはBuzz、両方の倍数である「15の倍数」のときはFizzBuzzと出力してください。
+
+def fizz_buzz
+  (1..100).each do |num|
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+end
+
+fizz_buzz
